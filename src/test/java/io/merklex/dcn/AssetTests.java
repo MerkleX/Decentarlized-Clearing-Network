83ccefcd08509266a370c50bbe7abc3d0a57038f
Patrick Lorio <patrick@merklex.io>
2018-09-12 17:32:02 -0700

    Remove redundant name is AssetTests

--- src/test/java/io/merklex/dcn/AssetTests.java
+++ src/test/java/io/merklex/dcn/AssetTests.java
@@ -60,78 +60,76 @@ public class AssetTests {
                 });
             });
 
-            describe("add assets", () -> {
-                StaticNetwork.DescribeCheckpoint();
+            StaticNetwork.DescribeCheckpoint();
 
-                DCN dcn = StaticNetwork.DCN();
-                String addr = "0xca35b7d915458ef540ade6068dfe2f44e8fa733c";
+            DCN dcn = StaticNetwork.DCN();
+            String addr = "0xca35b7d915458ef540ade6068dfe2f44e8fa733c";
 
-                describe("add first asset", () -> {
-                    Box<TransactionReceipt> receipt = new Box<>();
+            describe("add first asset", () -> {
+                Box<TransactionReceipt> receipt = new Box<>();
 
-                    it("add asset", () -> {
-                        receipt.value = dcn.add_asset("ABCD", BigInteger.ONE, addr).send();
-                    });
-
-                    it("should have log as first asset", () -> {
-                        List<Log> logs = receipt.value.getLogs();
-                        assertEquals(1, logs.size());
-                        Log log = logs.get(0);
-                        assertEquals("0x0001", log.getData());
-                        assertEquals(BigInteger.ONE, dcn.get_asset_count().send());
-                    });
-
-                    it("should not modify ether asset", () -> {
-                        Tuple3<String, BigInteger, String> asset = dcn.get_asset(BigInteger.valueOf(0)).send();
-                        assertEquals("ETH ", asset.getValue1());
-                        assertEquals(BigInteger.valueOf(100000000), asset.getValue2());
-                        assertEquals("0x0000000000000000000000000000000000000000", asset.getValue3());
-                    });
-
-                    it("should be able to query asset", () -> {
-                        Tuple3<String, BigInteger, String> asset = dcn.get_asset(BigInteger.valueOf(1)).send();
-                        assertEquals("ABCD", asset.getValue1());
-                        assertEquals(BigInteger.ONE, asset.getValue2());
-                        assertEquals(addr, asset.getValue3());
-                    });
+                it("add asset", () -> {
+                    receipt.value = dcn.add_asset("ABCD", BigInteger.ONE, addr).send();
                 });
 
-                describe("add second asset", () -> {
-                    Box<TransactionReceipt> receipt = new Box<>();
+                it("should have log as first asset", () -> {
+                    List<Log> logs = receipt.value.getLogs();
+                    assertEquals(1, logs.size());
+                    Log log = logs.get(0);
+                    assertEquals("0x0001", log.getData());
+                    assertEquals(BigInteger.ONE, dcn.get_asset_count().send());
+                });
 
-                    it("add asset", () -> {
-                        receipt.value = dcn.add_asset("ABC ", BigInteger.valueOf(231421), addr).send();
-                        ;
-                    });
+                it("should not modify ether asset", () -> {
+                    Tuple3<String, BigInteger, String> asset = dcn.get_asset(BigInteger.valueOf(0)).send();
+                    assertEquals("ETH ", asset.getValue1());
+                    assertEquals(BigInteger.valueOf(100000000), asset.getValue2());
+                    assertEquals("0x0000000000000000000000000000000000000000", asset.getValue3());
+                });
 
-                    it("should have log as second asset", () -> {
-                        List<Log> logs = receipt.value.getLogs();
-                        assertEquals(1, logs.size());
-                        Log log = logs.get(0);
-                        assertEquals("0x0002", log.getData());
-                        assertEquals(BigInteger.valueOf(2), dcn.get_asset_count().send());
-                    });
+                it("should be able to query asset", () -> {
+                    Tuple3<String, BigInteger, String> asset = dcn.get_asset(BigInteger.valueOf(1)).send();
+                    assertEquals("ABCD", asset.getValue1());
+                    assertEquals(BigInteger.ONE, asset.getValue2());
+                    assertEquals(addr, asset.getValue3());
+                });
+            });
 
-                    it("should not modify ether", () -> {
-                        Tuple3<String, BigInteger, String> asset = dcn.get_asset(BigInteger.valueOf(0)).send();
-                        assertEquals("ETH ", asset.getValue1());
-                        assertEquals(BigInteger.valueOf(100000000), asset.getValue2());
-                        assertEquals("0x0000000000000000000000000000000000000000", asset.getValue3());
-                    });
+            describe("add second asset", () -> {
+                Box<TransactionReceipt> receipt = new Box<>();
 
-                    it("should not modify asset 1", () -> {
-                        Tuple3<String, BigInteger, String> asset = dcn.get_asset(BigInteger.valueOf(1)).send();
-                        assertEquals("ABCD", asset.getValue1());
-                        assertEquals(BigInteger.ONE, asset.getValue2());
-                        assertEquals(addr, asset.getValue3());
-                    });
+                it("add asset", () -> {
+                    receipt.value = dcn.add_asset("ABC ", BigInteger.valueOf(231421), addr).send();
+                    ;
+                });
 
-                    it("should be able to query asset 2", () -> {
-                        Tuple3<String, BigInteger, String> asset = dcn.get_asset(BigInteger.valueOf(2)).send();
-                        assertEquals("ABC ", asset.getValue1());
-                        assertEquals(BigInteger.valueOf(231421), asset.getValue2());
-                        assertEquals(addr, asset.getValue3());
-                    });
+                it("should have log as second asset", () -> {
+                    List<Log> logs = receipt.value.getLogs();
+                    assertEquals(1, logs.size());
+                    Log log = logs.get(0);
+                    assertEquals("0x0002", log.getData());
+                    assertEquals(BigInteger.valueOf(2), dcn.get_asset_count().send());
+                });
+
+                it("should not modify ether", () -> {
+                    Tuple3<String, BigInteger, String> asset = dcn.get_asset(BigInteger.valueOf(0)).send();
+                    assertEquals("ETH ", asset.getValue1());
+                    assertEquals(BigInteger.valueOf(100000000), asset.getValue2());
+                    assertEquals("0x0000000000000000000000000000000000000000", asset.getValue3());
+                });
+
+                it("should not modify asset 1", () -> {
+                    Tuple3<String, BigInteger, String> asset = dcn.get_asset(BigInteger.valueOf(1)).send();
+                    assertEquals("ABCD", asset.getValue1());
+                    assertEquals(BigInteger.ONE, asset.getValue2());
+                    assertEquals(addr, asset.getValue3());
+                });
+
+                it("should be able to query asset 2", () -> {
+                    Tuple3<String, BigInteger, String> asset = dcn.get_asset(BigInteger.valueOf(2)).send();
+                    assertEquals("ABC ", asset.getValue1());
+                    assertEquals(BigInteger.valueOf(231421), asset.getValue2());
+                    assertEquals(addr, asset.getValue3());
                 });
             });
         });
